26653a79daa7f5dc10837b8b7fe6e14b99b76052
Mike McMahon <mike@mcmahon.com>
2014-01-25 16:36:01 -0800

    Much refactoring, many codes, wow, so fantastic!

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #include "SDL_Util.h"
 #include "Snake.h"
 
-#define DEFAULT_WIDTH 300
-#define DEFAULT_HEIGHT 300
+#define DEFAULT_WIDTH 312
+#define DEFAULT_HEIGHT 312
 
 typedef struct {
         unsigned int w;
@@ -31,8 +31,8 @@ typedef struct Food {
 void food_move(Food *food)
 {
         srand(time(NULL));
-        food->x = (rand() % (game_window.w - 10));
-        food->y = (rand() % (game_window.h - 10));
+        food->x = 12 * (rand() % 25);
+        food->y = 12 * (rand() % 25);
 }
 
 /**
@@ -41,7 +41,6 @@ void food_move(Food *food)
 void food_render(SDL_Surface *window_surf, Food *food)
 {
         SDL_Surface *surf;
-        SDL_Texture *text;
         SDL_Rect r;
 
         surf = SDL_CreateRGBSurface(0, 10, 10, 32,
@@ -49,15 +48,20 @@ void food_render(SDL_Surface *window_surf, Food *food)
                                     0x00FF0000,
                                     0x0000FF00,
                                     0x000000FF);
-        r.h = 8; r.w = 8;
-        r.x = 2; r.y = 2;
+        r.h = 8;
+        r.w = 8;
+        r.x = 2;
+        r.y = 2;
         SDL_FillRect(surf, &r, 0x66666666);
 
-        r.h = 8; r.w = 8;
-        r.x = 0; r.y = 0;
+        r.h = 8;
+        r.w = 8;
+        r.x = 0;
+        r.y = 0;
         SDL_FillRect(surf, &r, 0x666666FF);
 
-        r.w = 10; r.h = 10;
+        r.w = 10;
+        r.h = 10;
         r.x = food->x;
         r.y = food->y;
 
@@ -71,7 +75,7 @@ typedef enum {
         SNAKE_RIGHT,
         SNAKE_UP,
         SNAKE_DOWN
-} Snake_Move;
+} Snake_Direction;
 
 /**
  * Linked list of snake body parts
@@ -87,6 +91,7 @@ typedef struct Body {
  */
 typedef struct Snake {
         int size;
+        int increment;
         struct Body *body;
         SDL_Surface *surf;
 } Snake;
@@ -108,7 +113,7 @@ void snake_init_body(Body **out_body, SDL_Rect *rect, Body *prev)
  */
 void snake_create(SDL_Renderer *renderer, Snake *snake)
 {
-        SDL_Rect r = SDL_CreateRect(10, 10, 100, 100);
+        SDL_Rect r = SDL_CreateRect(10, 10, 156, 156);
 
         snake->size = 1;
         snake_init_body(&snake->body, &r, NULL);
@@ -189,7 +194,59 @@ SDL_Rect *snake_body_at(Snake *snake, int pos)
         return rect;
 }
 
-void snake_move(Body *body, Snake_Move dir, int incr)
+/**
+ * Checks to ensure the snake does not leave the bounds of the game
+ * and move freely about the cabin
+ */
+void snake_translate_body(SDL_Rect * body, Snake_Direction dir)
+{
+        if (body->x >= game_window.w && dir == SNAKE_RIGHT)
+                body->x = (body->x - game_window.w);
+
+        if (body->x < 0 && dir == SNAKE_LEFT)
+                body->x = game_window.w + body->x;
+
+        if (body->y >= game_window.h && dir == SNAKE_DOWN)
+                body->y = body->y - game_window.h;
+
+        if (body->y < 0 && dir == SNAKE_UP)
+                body->y = game_window.h + body->y;
+}
+
+/**
+ * Gets the next move made by the snake
+ */
+void snake_next_move(Snake *snake, Snake_Direction dir, SDL_Rect *out_rect)
+{
+        int x = 0;
+        int y = 0;
+        switch(dir) {
+        case SNAKE_LEFT:
+                x -= snake->increment;
+                break;
+        case SNAKE_UP:
+                y -= snake->increment;
+                break;
+        case SNAKE_RIGHT:
+                x += snake->increment;
+                break;
+        case SNAKE_DOWN:
+                y += snake->increment;
+                break;
+        }
+
+        *out_rect = snake->body->value;
+        out_rect->x += x;
+        out_rect->y += y;
+
+        snake_translate_body(out_rect, dir);
+}
+
+
+/**
+ * Moves the head of the snake forward the incr amount based on direction
+ */
+void snake_move(Body *body, Snake_Direction dir, int incr)
 {
         int x = 0;
         int y = 0;
@@ -211,20 +268,13 @@ void snake_move(Body *body, Snake_Move dir, int incr)
         body->value.x += x;
         body->value.y += y;
 
-        if (body->value.x >= game_window.w && dir == SNAKE_RIGHT)
-                body->value.x = (body->value.x - game_window.w);
-
-        if (body->value.x <= 0 && dir == SNAKE_LEFT)
-                body->value.x = game_window.w + body->value.x;
-
-        if (body->value.y >= game_window.h && dir == SNAKE_DOWN)
-                body->value.y = body->value.y - game_window.h;
-
-        if (body->value.y <= 0 && dir == SNAKE_UP)
-                body->value.y = game_window.h + body->value.y;
+        snake_translate_body(&(body->value), dir);
 }
 
-void snake_handle_input(Snake *snake, Snake_Move dir, int incr)
+/**
+ * Handles input for the snake
+ */
+void snake_handle_input(Snake *snake, Snake_Direction dir)
 {
         Body *body = snake->body->next;
 
@@ -238,7 +288,10 @@ void snake_handle_input(Snake *snake, Snake_Move dir, int incr)
                         body->value.x = body->prev->value.x;
                         body->value.y = body->prev->value.y;
                 } else {
-                        snake_move(body, dir, incr);
+                        // we really only move the head forward
+                        // Everything else is just copied to the location of
+                        // the piece before it
+                        snake_move(body, dir, snake->increment);
                 }
                 body = body->prev;
         }
@@ -252,13 +305,13 @@ void snake_handle_input(Snake *snake, Snake_Move dir, int incr)
  * accomplished by moving the snake forward one whole iteration
  * and then adding a new body piece at the end
  **/
-void snake_nibble(Snake *snake, int incr,
-                  Snake_Move dir)
+void snake_nibble(Snake *snake,
+                  Snake_Direction dir)
 {
         Body *body = snake_get_tail(snake->body);
         int x = body->value.x;
         int y = body->value.y;
-        snake_handle_input(snake, dir, incr);
+        snake_handle_input(snake, dir);
         SDL_Rect r = SDL_CreateRect(10, 10, x, y);
         snake_init_body(&body->next, &r, body);
         snake->size++;
@@ -289,7 +342,7 @@ int snake_free(Snake *snake)
 /**
  * Returns 0 if the snake collides with food, otherwise -1
  */
-int snake_collides(Snake *snake, Food *food)
+int snake_food_intersect(SDL_Rect *head, Food *food)
 {
         SDL_Rect rect;
         rect.x = food->x;
@@ -297,17 +350,35 @@ int snake_collides(Snake *snake, Food *food)
         rect.h = 10;
         rect.w = 10;
 
-        if (SDL_HasIntersection(&snake->body->value, &rect) == SDL_TRUE) {
+        if (SDL_HasIntersection(head, &rect) == SDL_TRUE) {
                 return 0;
         }
 
         return -1;
 }
 
+/**
+ * returns 0 if the snake collides with itself, otherwise -1
+ */
+int snake_self_intersect(Snake *snake, SDL_Rect *next_move)
+{
+        Body *abody = snake->body->next;
+
+        while (abody != NULL) {
+                if (SDL_HasIntersection(next_move, &abody->value) == SDL_TRUE) {
+                        return 0;
+                }
+                abody = abody->next;
+        }
+
+        return -1;
+}
+
 /******************** SDL ******************/
-void update();
+
+void update(Snake *, Snake_Direction, Food *);
 void render(float interpolation);
-int input(SDL_Event *e, Snake_Move *);
+int input(SDL_Event *e, Snake_Direction *);
 
 int main (int argc, char *args[])
 {
@@ -315,7 +386,7 @@ int main (int argc, char *args[])
         SDL_Renderer *renderer;
         SDL_bool quit = SDL_FALSE;
         SDL_Event e;
-        SDL_Rect draw_rect; //  = malloc(sizeof(struct SDL_Rect));
+        SDL_Rect draw_rect;
         SDL_Surface *window_surface;
         SDL_Texture *game_texture;
 
@@ -337,7 +408,7 @@ int main (int argc, char *args[])
         /** TIME KEEPING **/
 
         Snake snake;
-        Snake_Move cur_dir = SNAKE_LEFT;
+        Snake_Direction cur_dir = SNAKE_LEFT;
         float snake_fps = 10;
         float snake_movement = 12;
         float snake_ms_per_update = (1000 / snake_fps);
@@ -371,6 +442,7 @@ int main (int argc, char *args[])
         game_texture = SDL_CreateTextureFromSurface(renderer, window_surface);
 
         snake_create(renderer, &snake);
+        snake.increment = snake_movement;
 
         while (!quit) {
                 current = SDL_GetTicks();
@@ -382,23 +454,12 @@ int main (int argc, char *args[])
                 if (input(&e, &cur_dir) != 0)
                         quit = SDL_TRUE;
 
+                // Game runs at 60FPS snake runs at 10FPS
                 while (lag >= ms_per_update) {
                         snake_lag += ms_per_update;
 
-                        // Game runs at 60FPS snake runs at 10FPS
                         if (snake_lag >= snake_ms_per_update) {
-                                snake_handle_input(
-                                        &snake,
-                                        cur_dir,
-                                        snake_movement);
-
-                                if (snake_collides(&snake, &food) == 0) {
-                                        food_move(&food);
-                                        snake_nibble(
-                                                &snake,
-                                                snake_movement,
-                                                cur_dir);
-                                }
+                                update(&snake, cur_dir, &food);
                                 snake_lag -= snake_ms_per_update;
                         }
                         lag -= ms_per_update;
@@ -411,12 +472,15 @@ int main (int argc, char *args[])
 
                 for (int i = 1; i <= snake.size; i++) {
                         draw_rect = *snake_body_at(&snake, i);
-                        SDL_BlitSurface(snake.surf, NULL, window_surface, &draw_rect);
+                        SDL_BlitSurface(snake.surf, NULL,
+                                        window_surface, &draw_rect);
 
                 }
 
                 food_render(window_surface, &food);
-                SDL_UpdateTexture(game_texture, NULL, window_surface->pixels, window_surface->pitch);
+                SDL_UpdateTexture(game_texture, NULL,
+                                  window_surface->pixels,
+                                  window_surface->pitch);
                 SDL_RenderCopy(renderer, game_texture, NULL, NULL);
 
                 SDL_RenderPresent(renderer);
@@ -433,11 +497,35 @@ int main (int argc, char *args[])
         return 0;
 }
 
+void update(Snake *snake, Snake_Direction cur_dir, Food *food) {
+        SDL_Rect next_move;
+
+        snake_next_move(
+                snake,
+                cur_dir,
+                &next_move);
+
+        if (snake_food_intersect(&next_move, food) == 0) {
+                food_move(food);
+                snake_nibble(
+                        snake,
+                        cur_dir);
+        }
+
+        if (snake_self_intersect(snake, &next_move) == 0) {
+                printf("Snake collides!");
+        } else {
+                snake_handle_input(
+                        snake,
+                        cur_dir);
+        }
+}
+
 /**
  * Handles the input from the game and returns -1 if should quit
  * otherwise a zero that input was handled per usual
  */
-int input(SDL_Event *e, Snake_Move *direction)
+int input(SDL_Event *e, Snake_Direction *direction)
 {
         if (e->type == SDL_QUIT)
                 return -1;
@@ -445,14 +533,18 @@ int input(SDL_Event *e, Snake_Move *direction)
         if (e->type == SDL_KEYDOWN) {
                 if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_ESCAPE] != 0)
                         return -1;
-                else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_UP] != 0)
+                if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_UP] != 0) {
                         *direction = SNAKE_UP;
-                else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_RIGHT] != 0)
+                }
+                if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_RIGHT] != 0) {
                         *direction = SNAKE_RIGHT;
-                else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_LEFT] != 0)
+                }
+                if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_LEFT] != 0) {
                         *direction = SNAKE_LEFT;
-                else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_DOWN] != 0)
+                }
+                if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_DOWN] != 0) {
                         *direction = SNAKE_DOWN;
+                }
         }
 
         return 0;
